b682074e0898926f505b2aad40fd795f965e404e
jenishbhesaniya <52570404+jenishbhesaniya@users.noreply.github.com>
2021-06-17 09:59:50 +0530

    table show
    table details

--- src/app/states/states.component.ts
+++ src/app/states/states.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
-import { ShareddataService} from '../shared/shareddata.service'
+import { ShareddataService} from '../shared/shareddata.service';
+import {formatDate} from '@angular/common';
 export interface State {
   state_id:number
   state_name: string;
@@ -11,6 +12,15 @@ export interface State {
 export interface District {
   district_id:number
   district_name: string;
+}
+export interface Center{
+  center_id:number;
+  name:string;
+  address:string;
+  available_capacity:number;
+  min_age_limit:number;
+
+  slots:any;
 }
 @Component({
   selector: 'app-states',
@@ -29,6 +39,9 @@ export class StatesComponent implements OnInit {
   filteredDistrict!: Observable<District[]>;
   states: State[] = [] ;
   district:District[] = [];
+  center:Center[]=[];
+  displayedColumns: string[] = ['center_id', 'name', 'Avaiable'];
+  table:boolean=false;
   reqHeader = new HttpHeaders({
     'Content-Type': 'application/json',
     'Authorization': 'Bearer '+this.token.posToken()
@@ -88,8 +101,14 @@ export class StatesComponent implements OnInit {
       );
   }
 getdata(a:string){
-  console.log(a);
+  let date=formatDate(new Date(), 'dd/MM/yyyy', 'en');
+ console.log(date);
+  this.http.get<any>('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id='+a+'&date='+date).subscribe((data: any)=>{
+    this.center=data['sessions'];
+    console.log(this.center);
+    this.table=true;
+});
+
+
 }
-
-
 }
