4386482d1a17c51a2f0a70f2364845c32f7ba9ab
jenishbhesaniya <52570404+jenishbhesaniya@users.noreply.github.com>
2021-06-24 17:36:41 +0530

    tt

--- src/app/states/states.component.ts
+++ src/app/states/states.component.ts
@@ -48,6 +48,7 @@ export class StatesComponent implements OnInit {
   stName:string = '';
   stNdDst!:FormGroup;
   tiken!:string;
+  showProgress = false
   date:Array<Date>=[];
   dstSelect = true;
   filteredStates!: Observable<State[]>;
@@ -135,6 +136,7 @@ export class StatesComponent implements OnInit {
 
   }
 getdata(a:number){
+  this.showProgress = true
   let d= formatDate(new Date(),'dd/MM/yyy', 'en-in');
   console.log(d)
   this.http.get<any>('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id='+a+'&date='+d).subscribe((data: any)=>{
@@ -142,12 +144,13 @@ getdata(a:number){
     console.log(this.center);
     this.table=true;
      this.listlength=data['sessions'].length;
+     this.showProgress = false
 });
 
 
 }
 getpin(){
-
+  this.showProgress = true
   let d= formatDate(new Date(),'dd/MM/yyy', 'en-in');
   let pin = this.formpin.get('code')?.value;
   console.log(d,pin);
@@ -155,6 +158,7 @@ getpin(){
   this.center=data['sessions'];
   this.table=true;
   this.listlength=data['sessions'].length;
+  this.showProgress = false
 
   })
 }
