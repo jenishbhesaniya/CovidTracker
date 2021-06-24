13530c81839c89b2a8e23c43c2c3062c46fe940a
mangukiya12 <66070930+mangukiya12@users.noreply.github.com>
2021-06-24 18:01:21 +0530

    css

--- src/app/states/states.component.ts
+++ src/app/states/states.component.ts
@@ -50,6 +50,7 @@ export class StatesComponent implements OnInit {
   tiken!:string;
   date:Array<Date>=[];
   dstSelect = true;
+  showProgress = false;
   filteredStates!: Observable<State[]>;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   filteredDistrict!: Observable<District[]>;
@@ -135,6 +136,7 @@ export class StatesComponent implements OnInit {
 
   }
 getdata(a:number){
+  this.showProgress = true
   let d= formatDate(new Date(),'dd/MM/yyy', 'en-in');
   console.log(d)
   this.http.get<any>('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id='+a+'&date='+d).subscribe((data: any)=>{
@@ -142,12 +144,13 @@ getdata(a:number){
     console.log(this.center);
     this.table=true;
      this.listlength=data['sessions'].length;
+     this.showProgress = false;
 });
 
 
 }
 getpin(){
-
+  this.showProgress = true
   let d= formatDate(new Date(),'dd/MM/yyy', 'en-in');
   let pin = this.formpin.get('code')?.value;
   setInterval(()=>{
@@ -159,7 +162,7 @@ getpin(){
   this.center=data['sessions'];
   this.table=true;
   this.listlength=data['sessions'].length;
-
+  this.showProgress = false;
   })
 }
 
